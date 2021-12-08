9a9f2a956285c192694c1b3900fb3a915b6d360e
phil84mac <phil@thinkingbig.net>
2021-12-08 16:17:31 -0400

    commited

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,27 @@
-export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+import React, { useState, useEffect, MouseEvent, Component} from 'react';
+import { render } from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+  
+  <React.Fragment>
+      <div className="container-fluid header logoHeader">
+        <div className="row ">
+          <div className="col">
+            <Link href="/">
+            <img className="logo" src="/CAUSABLElogo.svg" alt="Causablelogo"/>
+            </Link>
+          </div>
+        </div>
+        </div>
+
+  </React.Fragment>
+
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import React, { useState, useRef, useEffect } from 'react';
+import Link from 'next/link'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useRouter } from 'next/router';
+import Button from 'react-bootstrap/Button';
+
 
 export default function Home() {
   return (
@@ -9,12 +17,15 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Container>
+        <h1>G-day</h1>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        </Container>
       </main>
 
       <Footer />
