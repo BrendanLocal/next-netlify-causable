e821c59c8d09a31ea9521b124ad29245ad106ea6
phil84mac <phil@thinkingbig.net>
2021-12-14 17:15:12 -0400

    landing 60% done

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
 
             <Col xl={6} lg={7} md={7} sm={12} className="px-xl-5 px-lg-5 pb-lg-5 mb-lg-5 pb-md-2 mb-md-2 px-3 mt-md-0 mt-2">
               <h1 className="text-white text-drop mb-3 mt-md-0 mt-5">Raise <br /> more with Causable</h1>
-              <p className="lead text-grey text-tight-drop pe-xl-4 ms-2 mb-4">Increase your charitable fundraising revenue with our all-in-one platform.</p>
+              <p className="lead text-grey text-tight-drop pe-xl-4 ms-1 mb-4">Increase your charitable fundraising revenue with our all-in-one platform.</p>
               <Row className="pb-md-5 pb-3 mb-xl-5 d-none d-sm-block">
                 <Col>
                   <Link href="#">
@@ -61,21 +61,21 @@ export default function Home() {
             </Col>
 
             <Col xl={6} lg={5} md={5} sm={12} className="p-xl-5 py-lg-5 px-lg-4 d-flex justify-content-center">
-              <Image fluid className="hero-img mt-5" src="/landing-mock.svg" alt="Image of Causable app" loading="lazy" />
+              <Image fluid className="hero-img mt-5 me-xl-5" src="/landing-mock.svg" alt="Image of Causable app" />
             </Col>
 
           </Row>
         </Container>
 
 
-        <Container fluid id="why" className="v-full bg-dgnl c-drag px-xl-5 pb-5">
-          <Row className="justify-content-center align-items-center d-flex px-md-5 px-4 pe-lg-0 pe-md-0 pb-5 mx-xl-5 mx-lg-4">
+        <Container fluid id="why" className="v-full bg-dgnl c-drag px-xl-5 pb-5 mb-5">
+          <Row className="align-items-center d-flex px-md-5 px-4 pe-lg-0 pe-md-0 pb-5 mx-xl-5 mx-lg-4 ">
 
             <Col xl={5} lg={5} md={5} sm={12} className="px-xl-5 ps-lg-4 pt-md-5 mb-lg-5 mb-md-1 mb-0 pb-md-5 pb-2 ">
               <h2 className="text-white text-drop mb-3">Why Causable?</h2>
               <p className="text-grey mb-3 bold ms-1 text-tight-drop">Causable’s electronic raffle technology and marketing platform provides your charitable organizations with unparalleled fundraising capacity in a secure and interactive manner.</p>
               <object className="d-block d-sm-none" type="image/svg+xml" data="/Causable-Animation-ENG.svg" alt="Causable platform animation" />
-              <p className="text-grey mb-4 ms-1 pe-xl-5 text-tight-drop">Our mobile playing app allows anyone in your area to play your electronic raffle via their mobile device or computer --  expanding your raffle’s reach and fundraising potential.</p>
+              <p className="text-grey mb-4 ms-1 text-tight-drop">Our mobile playing app allows anyone in your area to play your electronic raffle via their mobile device or computer --  expanding your raffle’s reach and fundraising potential.</p>
               <div className="d-none d-sm-block">
                 <Link href="#">
                   <Button className="btn btn-outline tight-drop d-arrow ms-1 mb-md-3 mb-0">
@@ -92,27 +92,157 @@ export default function Home() {
               </div>
             </Col>
 
-            <Col xl={7} lg={7} md={7} sm={12} className="mb-0 justify-content-center p-0">
+            <Col xl={7} lg={7} md={7} sm={12} className="mb-0 justify-content-center p-0 pb-xl-5 pb-md-5">
               <object className="d-none d-sm-block" type="image/svg+xml" data="/Causable-Animation-ENG.svg" alt="Causable platform animation" />
             </Col>
 
           </Row>
         </Container>
 
-        <Container fluid id="what" className="v-full z-0 bg-grad c-drag px-xl-5 pb-5">
-          <Row className="justify-content-center align-items-center d-flex px-md-5 px-4 pe-lg-0 pe-md-0 pb-5 mx-xl-5 mx-lg-4">
+        <Container fluid id="what" className="v-full z-0 bg-grad c-drag ps-xl-5 pb-5 mb-5">
 
-            <Col xl={5} lg={5} md={5} sm={12} className="px-xl-5 ps-lg-4 pt-md-5 mb-lg-5 mb-md-1 mb-0 pb-md-5 pb-2">
+          <Row id="what-1" className="align-items-center d-flex ps-md-5 ps-4 pe-lg-0 pe-md-0 pb-5 ms-xl-5 ms-lg-4">
+
+            <Col xl={5} lg={5} md={8} sm={12} className="px-xl-5 ps-lg-4 pt-md-5 mb-lg-5 mb-md-1 mb-0 pb-md-5 pb-2 pe-5">
               <h6 className="text-blue text-tight-drop mb-3 mt-5">WHAT YOU GET</h6>
               <h3 className="text-white text-drop mb-3">The Game</h3>
               <p className="text-grey text-tight-drop mb-3 bold">Causables electronic raffle technology and marketing platform provides your charitable organizations with unparalleled fundraising capacity in a secure and interactive manner.</p>
               <p className="text-grey text-tight-drop mb-3">Users can sign up, buy their numbers and play each week. The platform uses a random number generator to select a number for each draw and notifies the ticket holder. The platform is secure and eliminates financial errors, the need for cumbersome accounting, and the need for physical ticket sales and tracking.</p>
             </Col>
 
-            <Col xl={7} lg={7} md={7} sm={12} className="p-0 offpage">
-            <Image fluid className="dash-img" src="/goldrush-dash.svg" alt="50/50 results" loading="lazy" />
+            <Col md={12} sm={12} className="p-0 d-block d-lg-none">
+            <Image fluid className="dash-img p-0" src="/goldrush-dash.svg" alt="50/50 results" />
+            </Col>
+
+            <Col lg={7} className="p-0 d-none d-lg-block">
+            <Image fluid className="dash-img p-0" src="/goldrush-dash.svg" alt="50/50 results" />
             </Col>
             
+          </Row>
+
+          <Row id="what-2" className="align-items-center d-flex ps-md-5 ps-4 pe-lg-0 pe-md-0 pb-5 ms-xl-5 ms-lg-4">
+
+            <Col xl={5} lg={5} md={8} sm={12} className="px-xl-5 ps-lg-4 pt-md-5 mb-lg-5 mb-md-1 mb-0 pb-md-5 pb-2 pe-5">
+              <h6 className="text-blue text-tight-drop mb-3 mt-5">WHAT YOU GET</h6>
+              <h3 className="text-white text-drop mb-3">Branded Mobile App for your Charity</h3>
+              <p className="text-grey text-tight-drop mb-3 bold">Logo, colors, important communication, this is <span className="italic">your</span> charity app.</p>
+              <p className="text-grey text-tight-drop mb-3">We incorporate your charity/team branding and feature your important content for communicating with your community. News, schedules, games, rosters, events, etc are all featured on your app and provide a place to communicate and engage with your members / fans / stakeholders.</p>
+            </Col>
+
+            <Col md={12} sm={12} className="p-0 d-block d-lg-none">
+              <Row className="p-0 m-0">
+                <Col className="p-0 m-0">
+                <Image fluid className="app-img" src="/theapp1.svg" alt="Branded mobile app dashboard" />
+                </Col>
+                <Col className="px-3 m-0">
+                <Image fluid className="app-img" src="/theapp2.svg" alt="Branded mobile app schedule" />
+                </Col>
+                <Col className="p-0 m-0">
+                <Image fluid className="app-img-3" src="/theapp3.svg" alt="Branded mobile app loadscreen" />
+                </Col>
+              </Row>
+            </Col>
+             
+            <Col lg={7} className="p-0 d-none d-lg-block">
+            <Row className="p-0 m-0">
+                <Col className="p-0 m-0">
+                <Image fluid className="app-img" src="/theapp1.svg" alt="Branded mobile app dashboard" />
+                </Col>
+                <Col className="px-3 m-0">
+                <Image fluid className="app-img" src="/theapp2.svg" alt="Branded mobile app schedule" />
+                </Col>
+                <Col className="p-0 m-0">
+                <Image fluid className="app-img-3" src="/theapp3.svg" alt="Branded mobile app loadscreen" />
+                </Col>
+              </Row>
+            </Col>
+            
+          </Row>
+
+          <Row id="what-3" className="align-items-center d-flex py-5 ps-md-5 ps-4 pe-lg-0 pe-md-0 ms-xl-5 ms-lg-4">
+            <Col xl={5} lg={5} md={8} sm={12} className="px-xl-5 ps-lg-4 pt-md-5 mb-lg-5 mb-md-1 mb-0 pb-2 pe-lg-4 pe-5">
+              <h6 className="text-blue text-tight-drop mb-3 mt-5">WHAT YOU GET</h6>
+              <h3 className="text-white text-drop mb-3">Ongoing Support</h3>
+              <ul className="text-grey">
+                <li className="mb-2">
+                Technical Support
+                </li>
+                <li className="mb-2">
+                Marketing &amp; Creative 
+                </li>
+                <li className="mb-2">
+                Regulatory Compliance
+                </li>
+                <li className="mb-4">
+                Financial Reporting eliminates the huge volunteer effort required for manual lotteries, and ensures accuracy in management of funds, banking and financial reporting
+                </li>
+              </ul>
+              <div className="d-none d-sm-block">
+                <Link href="#">
+                  <Button className="btn btn-outline tight-drop d-arrow ms-1 mb-0">
+                    HOW TO WORK WITH US
+                  </Button>
+                </Link>
+              </div>
+              <div className="d-grid gap-2 mb-5">
+                <Link href="#">
+                  <Button className="btn btn-outline tight-drop d-arrow ms-2 pb-5 mb-0 d-block d-sm-none">
+                  HOW TO WORK WITH US
+                  </Button>
+                </Link>
+              </div>
+            </Col>
+
+            <Col md={12} sm={12} className="p-0 m-0 d-block d-lg-none">
+            <Image fluid className="support-img p-0" src="/ongoing-support.svg" alt="50/50 results" />
+            </Col>
+
+            <Col lg={7} className="p-0 d-none d-lg-block">
+            <Image fluid className="support-img p-0" src="/ongoing-support.svg" alt="50/50 results" />
+            </Col>            
+          </Row>  
+        </Container>
+
+        <Container fluid id="how" className="v-150 bg-dgnl c-drag-2 px-xl-5 pb-5 mb-5">
+          <Row className="align-items-center d-flex px-md-5 px-4 pe-lg-0 pe-md-0 py-5 mb-5 mx-xl-5 mx-lg-4">
+
+            <Col lg={5} md={6} sm={12} className="ps-xl-5 ps-lg-4 pe-lg-0 pe-md-4 py-md-5 mb-md-0 mb-3">
+              <h2 className="text-white text-drop mb-3">How To Work With Us</h2>
+              <p className="text-grey mb-3 bold ms-1 text-tight-drop">We have created an innovative solution that will help change the way charities engage their donors. The elegance and ease of use will delight your users and drive revenues to your organization.</p>
+              <p className="text-grey text-tight-drop mb-2">Along with our partnerships, our business model purposely has created an environment that ensures Causable has a vested interest in your success, a true partnership.</p>
+              <p className="text-grey text-tight-drop mb-2">With this innovation comes technical and business complexity. To help us navigate the complexity and create this elegant experience we have partnered with best in class industry leaders for their respective fields - banking, security, identity management, and compliance.</p>
+              <p className="text-grey text-tight-drop">For Causable, taking this route comes with inherent costs &amp; overhead, but it also gives stability, peace of mind and most importantly a clear path to success for your organization and for Causable.</p>
+            </Col>
+
+            <Col lg={{ span: 6, offset: 1 }} md={5} sm={12} className="well ms-lg-5 mb-md-0 mb-5">
+              <h4 class="text-white">It's so easy to get started!</h4>
+              <hr />
+              <h6 class="text-blue">STEP 1</h6>
+              <p class="lead text-grey mb-3">Partner with Causable</p>
+              <div className="d-none d-sm-block">
+                <Link href="#">
+                  <Button className="btn btn-blue tight-drop arrow">
+                    GET STARTED
+                  </Button>
+                </Link>
+              </div>
+              <div className="d-grid gap-2 mb-5">
+                <Link href="#">
+                  <Button className="btn btn-blue tight-drop arrow d-block d-sm-none">
+                    GET STARTED
+                  </Button>
+                </Link>
+              </div>
+              <hr />
+              <h6 class="text-blue">STEP 2</h6>
+              <p class="lead text-grey mb-2">Get your lottery license</p>
+              <p class="text-grey mb-5"><span className="bold">Don’t worry!</span> We will help you with that.</p>
+              <hr />
+              <h6 class="text-blue">STEP 3</h6>
+              <p class="lead text-grey">Start promoting</p>
+              
+            </Col>
+
           </Row>
         </Container>
 
